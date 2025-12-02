02c47cf43c8e25f2d4abf9276d3b326b6a20966d
Sam Hughes <samuel.e.hughes@gmail.com>
2025-12-02 13:42:45 -0700

    Bsics Setup

--- test.py
+++ test.py
@@ -4,3 +4,15 @@ print("Practice makes perfect.")
 print("Let's start coding!")
 
 print("Sam_testing")
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8000
+
+class Handler(SimpleHTTPRequestHandler):
+    pass
+
+if __name__ == "__main__":
+    print(f"Serving website at http://localhost:{PORT}")
+    server = HTTPServer(("localhost", PORT), Handler)
+    server.serve_forever()
